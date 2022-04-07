d92de007afe7e19a2d15a24bdc024b7283ca5637
laianehoger <55981086+laianehoger@users.noreply.github.com>
2022-04-07 08:22:40 -0300

    Add files via upload

---
+++ exe4.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<!-- Elabore um formulário utilizando o método GET, que recebe
+um número inteiro e escreve na tela se o número é par ou
+ímpar. Faça a validação para garantir que os campos foram
+preenchidos -->
+	<form method="GET">
+		<input type="number" name="numero">
+		<input type="submit" name="enviar" value="verificar">
+	</form>
+	<?php
+		if (isset($_GET['enviar'])){
+			$numero = $_GET['numero'];
+			if (isset($numero) && empty($numero) == false){
+				if ($numero % 2 == 0){
+					echo("Numero $numero é par");
+				}
+				else{
+					echo("Numero $numero é impar");
+				}
+			}
+		}
+	?>	
+</body>
+</html>
